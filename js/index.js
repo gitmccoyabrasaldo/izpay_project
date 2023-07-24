eda94365dbfe6a35b11bde9d80546f11c7accfaa
gitmccoyabrasaldo <mccoyabrasaldo@gmail.com>
2023-07-24 15:10:14 +0800

    Added Sections: - banner - partner (components) - promotions - merchants

--- js/index.js
+++ js/index.js
@@ -1,17 +1,9 @@
-let sidebar = document.getElementById("sideBar");
-let sidebarOverlay = document.getElementById("sideBarOverlay");
-let sideMenu1 = document.getElementById("sideMenu1");
-let sideMenu2 = document.getElementById("sideMenu2");
+// Call header components to all pages
+// Reusing the same header across multiple pages
+$(function () {
+  $("#header").load("/components/header.html");
+});
 
-function toggleSidebar() {
-  sidebar.classList.toggle("open-sidebar");
-  sidebarOverlay.classList.toggle("open-overlay");
-}
-
-function toggleSubMenu1() {
-  sideMenu1.classList.toggle("open-menu1");
-}
-
-function toggleSubMenu2() {
-  sideMenu2.classList.toggle("open-menu2");
-}
+$(function () {
+  $("#partner").load("/components/partner.html");
+});
