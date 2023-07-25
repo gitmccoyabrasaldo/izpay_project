185b998ee048e2146057a6bfaf31cd2ee1ba36fb
gitmccoyabrasaldo <mccoyabrasaldo@gmail.com>
2023-07-26 03:50:38 +0800

    Added Section: - Service - Testimonial
    Added assets/svg
    Added components/service

--- js/index.js
+++ js/index.js
@@ -4,6 +4,12 @@ $(function () {
   $("#header").load("/components/header.html");
 });
 
+// Reusing the same partner section across multiple pages
 $(function () {
   $("#partner").load("/components/partner.html");
+});
+
+// Reusing the same service section across multiple pages
+$(function () {
+  $("#service").load("/components/service.html");
 });
