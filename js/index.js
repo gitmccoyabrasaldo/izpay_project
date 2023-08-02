5c838da8be8d407cf21c32e97c0327d01176c834
gitmccoyabrasaldo <mccoyabrasaldo@gmail.com>
2023-08-02 15:07:03 +0800

    Added about page

--- js/index.js
+++ js/index.js
@@ -13,3 +13,40 @@ $(function () {
 $(function () {
   $("#service").load("/components/service.html");
 });
+
+// Reusing the same newsletter section across multiple pages
+$(function () {
+  $("#newsletter").load("/components/newsletter.html");
+});
+
+// Reusing the same footer section across multiple pages
+$(function () {
+  $("#footer").load("/components/footer.html");
+});
+
+// Reusing the same achievement section across multiple pages
+$(function () {
+  $("#achievement").load("/components/achievement.html");
+});
+
+// FOR UP COUNTER numbers
+// let valueDisplay = document.querySelectorAll(".number");
+
+// let interval = 4000;
+
+// valueDisplay.forEach((valueDisplay) => {
+//   let startVal = 0;
+//   let endVal = parseInt(valueDisplay.getAttribute("data-val"));
+//   // console.log(endVal);
+
+//   let duration = Math.floor(interval / endVal);
+
+//   let counter = setInterval(function () {
+//     startVal += 1;
+//     valueDisplay.textContent = startVal;
+
+//     if (startVal == endVal) {
+//       clearInterval(counter);
+//     }
+//   }, duration);
+// });
