5cddeb9bc302dfcc4039a7725bd22a0302da3b26
gitmccoyabrasaldo <mccoyabrasaldo@gmail.com>
2023-08-15 17:45:27 +0800

    - [x] #7 - [x] #6 - [x] Added navigation links footer

--- components/header.js
+++ components/header.js
@@ -17,3 +17,75 @@ function toggleSubMenu1() {
 function toggleSubMenu2() {
   sideMenu2.classList.toggle("open-menu2");
 }
+
+// Navbarrr
+$(function () {
+  let navLinks = $(".links a");
+  let dropdownMenuItems = $(".dropdown_menu a");
+
+  navLinks.on("click", function (event) {
+    navLinks.removeClass("active");
+    dropdownMenuItems.removeClass("active");
+
+    $(this).addClass("active");
+
+    const targetURL = $(this).attr("href");
+    window.location.href = targetURL;
+
+    event.preventDefault();
+  });
+
+  // Set the active link on page load
+  const currentURL = window.location.href;
+  navLinks.each(function () {
+    if (currentURL.includes($(this).attr("href"))) {
+      $(this).addClass("active");
+      return false; // Stop iteration once an active link is found
+    }
+  });
+
+  // Add active class to dropdown menu item and its parent "dropdown a" on page load
+  dropdownMenuItems.each(function () {
+    if (currentURL.includes($(this).attr("href"))) {
+      $(this).addClass("active");
+      $(this).closest(".dropdown").find(".eee").addClass("active"); // Add active class to parent dropdown "a"
+      return false; // Stop iteration once an active dropdown item is found
+    }
+  });
+});
+
+// Sidebar
+$(function () {
+  let sideLinks = $(".sidebar .links a");
+  let sub_menu = $(".sub_menu li a");
+
+  sideLinks.on("click", function (event) {
+    sideLinks.removeClass("active");
+    sub_menu.removeClass("active");
+
+    $(this).addClass("active");
+
+    const targetURL = $(this).attr("href");
+    window.location.href = targetURL;
+
+    event.preventDefault();
+  });
+
+  // Set the active link on page load
+  const currentURL = window.location.href;
+  sideLinks.each(function () {
+    if (currentURL.includes($(this).attr("href"))) {
+      $(this).addClass("active");
+      return false; // Stop iteration once an active link is found
+    }
+  });
+
+  // Add active class to dropdown menu item and its parent "dropdown a" on page load
+  sub_menu.each(function () {
+    if (currentURL.includes($(this).attr("href"))) {
+      $(this).addClass("active");
+      $(this).closest(".menu").find(".ee").addClass("active"); // Add active class to parent dropdown "a"
+      return false; // Stop iteration once an active dropdown item is found
+    }
+  });
+});
